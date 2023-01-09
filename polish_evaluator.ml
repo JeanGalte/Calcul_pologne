ca2a6199a460cb6579ffe47631b69837ba46057d
JeanGalte <102584320+JeanGalte@users.noreply.github.com>
2023-01-09 14:38:06 +0100

    Update polish_evaluator.ml

--- polish_evaluator.ml
+++ polish_evaluator.ml
@@ -1,33 +1,32 @@
+open Pervasives
 open Set
 open String
 open List
 open Char
 open Float
+open Printf
 
 exception BadExp of string 
 exception UnkwnownOp of string
-
 
 type symbole = float 
 (* Un opérateur peut être unaire ou bien binaire sur une calculatrice classique. Dans les 2 cas il prend un ou plusieurs arguments mais ne renvoie qu'un seul symbole *)
 type unaire = (float -> float)
 type binaire= (float -> float -> float)
+type famille_op = float -> float -> (float -> float -> float) -> float
 type operateur =  Unaire of unaire| Binaire of binaire | Swap | Dup
 type mix = Operateur of operateur | Symbole of symbole
 
 module SS1 = Set.Make(Char)
 module SS2 = Set.Make(String) 
-module SS3 = Set.Make(String)
-module SS4 = Set.Make(String)
-
 
 let chiffres = SS1.of_list ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9'] 
 let constantes = SS2.of_list ["pi";"e"]
-let binop = SS3.of_list ["+";"-";"/";"*";"POW"] 
-let unop = SS4.of_list ["NEG";"SIN";"COS";"TAN";"COSH";"SINH";"TANH";"ARCSIN";"ARCOS";"ARCTAN";"EXP";"LN";"LOG10";"SQRT"] 
+let binop = SS2.of_list ["+";"-";"/";"*";"POW"] 
+let unop = SS2.of_list ["NEG";"SIN";"COS";"TAN";"COSH";"SINH";"TANH";"ARCSIN";"ARCOS";"ARCTAN";"EXP";"LN";"LOG10";"SQRT"] 
 
 (* Dictionnaires eco+ *)
- let assoc_bin (s : string) : binaire=
+ let assoc_bin (s : string) : binaire =
 	match s with 
 	| "*" ->  ( *. )
 	| "-" -> ( -. )
@@ -38,7 +37,7 @@ let unop = SS4.of_list ["NEG";"SIN";"COS";"TAN";"COSH";"SINH";"TANH";"ARCSIN";"A
 
 let assoc_un (s : string) : unaire=
 	match s with
-	| "NEG" -> ( -. ) 0. 
+	| "NEG" -> ( -. ) 0.  
 	| "SIN" -> Float.sin
 	| "COS" -> Float.cos
 	| "TAN" -> Float.tan
@@ -65,8 +64,8 @@ let parse (s : string) =
         | [] -> List.rev acc
         | x::xs when SS1.mem x.[0] chiffres -> (Symbole (float_of_string x)) :: acc |> p xs
         | x::xs when SS2.mem x constantes -> (Symbole (assoc_const x)) :: acc |> p xs 
-        | x::xs when SS3.mem x binop -> (Operateur (Binaire (assoc_bin x))) :: acc |> p xs
-        | x::xs when SS4.mem x unop -> ((Operateur (Unaire (assoc_un x))) :: acc) |> p xs 
+        | x::xs when SS2.mem x binop -> (Operateur (Binaire (assoc_bin x))) :: acc |> p xs
+        | x::xs when SS2.mem x unop -> ((Operateur (Unaire (assoc_un x))) :: acc) |> p xs 
         | "DUP"::xs -> (Operateur Dup) :: acc |> p xs 
         | "SWAP"::xs -> (Operateur Swap) :: acc |> p xs
         | x::xs -> UnkwnownOp ("Élément inconu : " ^ x) |> raise
